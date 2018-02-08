cadc130d90a207d0522e437c78949a15b6c548b4
dthierry <dmolinat@andrew.cheme.cmu.edu>
2018-02-07 20:27:41 -0500

    changes to the bash run script

--- testing/script.sh
+++ testing/script.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-START_SESSION=15
-END_SESSION=19
+ROUND=6
+START_SESSION=1
+END_SESSION=3
 
 for ((i=$START_SESSION; i<$END_SESSION; i++))
-	do
-		mkdir ./run$i
-		cd ./run$i
-		git clone https://github.com/dthierry/nmpc_mhe_q.git -b bfb
-		cd ./nmpc_mhe_q/
-		virtualenv dav_$i
-		source ./dav_$i/bin/activate
-		pip install -r requirements.txt
-		pip install -e .
-		deactivate
-		cd ../..
+        do
+                mkdir ./run$i
+                cd ./run$i
+                git clone https://github.com/dthierry/nmpc_mhe_q.git -b bfb
+                cd ./nmpc_mhe_q/
+                virtualenv d$ROUND\_$i
+                source ./d$ROUND\_$i/bin/activate
+                pip install -r requirements.txt
+                pip install -e .
+                deactivate
+                cd ../..
 
-	done
+        done
 
 for ((i=$START_SESSION; i<$END_SESSION; i++))
-	do
-		tmux new -s dav_$i -d
-		tmux send-keys -t dav_$i "cd ./run$i" C-m
-		tmux send-keys -t dav_$i "cd ./nmpc_mhe_q" C-m
-		tmux send-keys -t dav_$i "source ./dav_$i/bin/activate" C-m
-		tmux send-keys -t dav_$i "cd ./testing/" C-m
-		tmux send-keys -t dav_$i "python tst_algv3_s1600_n4.py && date" C-m
-	done
+        do
+                tmux new -s d$ROUND\_$i -d
+                tmux send-keys -t d$ROUND\_$i "cd ./run$i" C-m
+                tmux send-keys -t d$ROUND\_$i "cd ./nmpc_mhe_q" C-m
+                tmux send-keys -t d$ROUND\_$i "source ./d$ROUND\_$i/bin/activate" C-m
+                tmux send-keys -t d$ROUND\_$i "cd ./testing/" C-m
+                tmux send-keys -t d$ROUND\_$i "python tst_algv3_s1600as_n0.py && date" C-m
+        done
+
 
